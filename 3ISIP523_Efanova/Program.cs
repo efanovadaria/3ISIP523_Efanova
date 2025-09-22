7496e38572ad5f9c22d74eb63167f735ccaabb59
Efanova Daria <efanova7335@mail.ru>
2025-09-22 17:32:08 +0300

    написала категории и класс

--- 3ISIP523_Efanova/Program.cs
+++ 3ISIP523_Efanova/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace StoreApp
+{
+    enum Category
+    {
+        Food, Pets, Clothes
+    }
+    class Product
+    {
+        private static int nextID = 1;
+
+        public int Code { get; private set; }
+        public string Name { get; private set; }
+        public double Prise { get; private set; }
+        public int Quantity { get; private set; }
+        public bool InStock => Quantity > 0;
+        public Category Category { get; private set; }
+
+    }
+}
