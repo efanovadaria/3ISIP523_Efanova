dd63be34085dfcecd3a16587c307aac14988ad47
Efanova Daria <efanova7335@mail.ru>
2025-11-01 12:02:37 +0300

    Базовая структура проекта и перечисление категорий

--- 3ISIP523_Efanova/Program.cs
+++ 3ISIP523_Efanova/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace StoreApp
+{
+    enum Category
+    {
+        Food, Pets, Clothes
+    }
+    class Product
+    {
+        private static int nextID = 1;
+
+        public int Code;
+        public string Name;
+        public double Prise;
+        public int Quantity;
+        public bool InStock => Quantity > 0;
+        public Category Category;
+
+        public Product(string name, double price, int quantity, Category category)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Название товара не может быть пустым");
+            if (price <= 0)
+                throw new ArgumentException("Цена должна быть больше нуля");
+            if (quantity < 0)
+                throw new ArgumentException("Количество не может быть отрицательным");
+
+            Code = nextID++;
+            Name = name;
+            Prise = price;
+            Quantity = quantity;
+            Category = category;
+
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Код: {Code} | Название: {Name} | Цена: {Prise} руб. | " + $"Количество: {Quantity} | В наличии: {(InStock ? "ДА" : "НЕТ")} | Категория: {Category}");
+        }
+
+        public void AddStock(int amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Количество не может быть отрицательным");
+            Quantity += amount;
+        }
+        public void Sell(int amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentException("Количество должно быть положительным");
+            if (Quantity < amount)
+                throw new ArgumentException("Недостаточно товара на складе");
+            Quantity -= amount;
+        }
+    }
+}
