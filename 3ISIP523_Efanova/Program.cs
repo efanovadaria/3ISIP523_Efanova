4da74add894949b9b56cfbf4b997341715bd9b2f
Efanova Daria <efanova7335@mail.ru>
2025-11-01 12:59:53 +0300

    функция списка и поиска

--- 3ISIP523_Efanova/Program.cs
+++ 3ISIP523_Efanova/Program.cs
@@ -334,7 +334,7 @@ namespace StoreApp
 
                 foreach (var item in products)
                 {
-                    item.PrintInfo();
+                    item.PrintInfo();2
                 }
             }
         }
